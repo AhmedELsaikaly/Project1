9e5c958f05cfe31580ae253f5637ec81c26dfedd
AhmedELsaikaly <ahmedelsaikaly@gmail.com>
2020-05-17 14:11:54 +0300

    projectAhmedRula

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+$("#inline_content input[id ="male"]").click(function(){
+    $("#result").text("saad");
+})
+// $("#male").click(function(){
+// if($("#male").prop("checked")=== true){
+// $("#result").text("saad")	
+// }
+// })
+
