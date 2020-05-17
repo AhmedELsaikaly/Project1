8f045d88e6fd47b1d514f6e11c3b5cc5f3167b12
RulaAbuHasna <rulahasna99@gmail.com>
2020-05-17 17:22:53 +0300

    modified

--- script.js
+++ script.js
@@ -1,12 +1,65 @@
 
+$("#btn").click(function(){
 
+var count=0;
+
+if($("#hend").prop("checked")=== true){
+	count=count+1;
+}
+if($("#khaled").prop("checked")=== true||$("#khale").prop("checked")=== true||$("#khal").prop("checked")=== true){
+	count=count+1;
+}
+if($("#karam").prop("checked")=== true){
+	count=count+1;
+}
+if($("#spain").prop("checked")=== true){
+	count=count+1;
+}
+if($("#sara").prop("checked")=== true){
+	count=count+1;
+}
+if($("#akram").prop("checked")=== true){
+	count=count+1;
+}
+if($("#hind").prop("checked")=== true){
+	count=count+1;
+}
+if($("#abuShaban").prop("checked")=== true){
+	count=count+1;
+}
+if($("#maher").prop("checked")=== true){
+	count=count+1;
+}
+if($("#leo").prop("checked")=== true){
+	count=count+1;
+}
+if($("#kareemm").prop("checked")=== true){
+	count=count+1;
+}
+if($("#safaa").prop("checked")=== true){
+	count=count+1;
+}
+if($("#hillis").prop("checked")=== true){
+	count=count+1;
+}
+if($("#Abd").prop("checked")=== true){
+	count=count+1;
+}
+if($("#Anas").prop("checked")=== true){
+	count=count+1;
+}
+if($("#Ibrahem").prop("checked")=== true){
+	count=count+1;
+}
+if($("#Altayeb").prop("checked")=== true){
+	count=count+1;
+}
+
+$('h1').text(count);
+	
+})
 /*
-$("#inline_content input[id ="male"]").click(function(){
-    $("#result").text("saad");
-})*/
-// $("#male").click(function(){
-// if($("#male").prop("checked")=== true){
-// $("#result").text("saad")	
-// }
-// })
-//////weplg[korgoerkgoer/]
+ if($("#hend").prop("checked")=== false&&$("#sara").prop("checked")=== false&&$("#mhmd").prop("checked")=== false){
+	alert("pick someone!");
+}
+*/
