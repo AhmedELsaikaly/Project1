f1ac97149236346d023a82ce089881a67484a9b6
RulaAbuHasna <rulahasna99@gmail.com>
2020-05-17 14:32:33 +0300

    first

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 
-
+/*
 $("#inline_content input[id ="male"]").click(function(){
     $("#result").text("saad");
-})
+})*/
 // $("#male").click(function(){
 // if($("#male").prop("checked")=== true){
 // $("#result").text("saad")	
